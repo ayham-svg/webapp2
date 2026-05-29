eac8c8a01272f03a9a3107fb0947ce91bb8643e4
ayham-svg <hezeirinya@gmail.com>
2026-05-29 11:57:01 +0200

    login php basis opgezet

---
+++ php/login.php
@@ -0,0 +1,14 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Lano & Ayham Travels - Login</title>
+    <link rel="stylesheet" href="../css/style.css" />
+    <link rel="stylesheet" href="../css/login.css" />
+  </head>
+  <body>
+
+  </body>
+</html>
